fc5ae1e45429b2d4f4d6c7fc50b950f004764d2f
oritzau <oritzau2021@gmail.com>
2022-11-29 10:45:18 -0500

    fixed wrong labels on days

--- calendar_project.py
+++ calendar_project.py
@@ -1,19 +1,16 @@
-#returns first day of given year
+
 def first_day_of_year(year):
     return int((2 + (year-1901) + (year-1901)/4) % 7)
 
-#returns day in words
 def day_num_to_words(day_num):
     day_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     day_in_words = day_list[day_num]
     return day_in_words
 
-#returns month in words
 def month_num_to_words(month_num):
     month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     return month_list[month_num - 1]
 
-#returns first day of month as number
 def first_day_month_in_year(year, month):
     first_day_of_year = int((2 + (year-1901) + (year-1901)/4) % 7)
     days_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -32,7 +29,6 @@ def first_day_month_in_year(year, month):
             counter += 1
     return first_day_month_in_year % 7
 
-#returns number of days in month
 def num_of_days(month_num, year):
     days_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month_num != 2:
@@ -42,8 +38,26 @@ def num_of_days(month_num, year):
             return 29
         else:
             return days_in_month_list[month_num - 1]
+def print_calendar(day_num, days_in_month_num):
+    top_str = month_num_to_words(month) + ', ' + str(year)
+    top_str = f'{top_str:^22}'
+    print(top_str)
+    calendar_str = 'Su Mon Tu We Th Fr Sa'
+    print(calendar_str)
+    day_printed = 1
+    counter = 1
+    space = ''
+    while counter < day_num:
+        print(f'{space:3}', end = '')
+        counter += 1
+    while counter >= day_num and day_printed <= days_in_month_num:
+        if counter % 7 == 0:
+            print(f'{day_printed:3}')
+        else:
+            print(f'{day_printed:3}', end = '')
+        counter += 1
+        day_printed += 1
 
-#takes year and month as arguments and prints calendar info
 if __name__ == "__main__":
     month = int(input('Month: '))
     while month < 0 or month > 12:
@@ -52,8 +66,5 @@ if __name__ == "__main__":
     year = int(input('Year: '))
 
     first_day = first_day_of_year(year)
-    print('\n')
-    print(f'The first day of {month_num_to_words(month)} is on a {day_num_to_words(first_day_month_in_year(year, month))} \n')
-    print(f'The first day of {year} is on a {day_num_to_words(first_day)}\n')
-    print(f'{month_num_to_words(month)} has {num_of_days(month,year)} days')
- 
+    print()
+    print_calendar(first_day_month_in_year(year, month), num_of_days(month, year))
